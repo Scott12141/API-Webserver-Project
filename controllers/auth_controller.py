ae576fefa368bc14b84608f7b2b262a260900333
Scott12141 <12141@coderacademy.edu.au>
2023-07-14 18:26:19 +1000

    Create Login with token

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import create_access_token
 from models.user import User, user_schema, users_schema
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
+from datetime import timedelta
 
 
 auth_bp = Blueprint('auth', __name__, url_prefix = '/auth')
@@ -24,10 +25,26 @@ def auth_register():
 
         db.session.add(user)
         db.session.commit()
-
+        # Now send a message to the front end with what data was stored in the back end(database)
         return user_schema.dump(user), 201
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {'error': 'This email address has already been registered please use another one'}, 409
         elif err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return {'error': f'Your {err.orig.diag.column_name} is required for registration'}, 409
+
+
+
+@auth_bp.route('/login', methods = ['POST'])
+def auth_login():
+    # retrieve data posted from the front end body in json format and store it in body_data
+    body_data = request.get_json()
+    # Query the database to find a user by their email address
+    qry = db.select(User).filter_by(email = body_data.get('email'))
+    user = db.session.scalar(qry)
+    if user and bcrypt.check_password_hash(user.password, body_data.get('password')):
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=7))
+        return {'email': user.email, 'token': token}
+    else:
+        return {'error': "email does not exist or password was incorrect, please try again"}, 401
+    
